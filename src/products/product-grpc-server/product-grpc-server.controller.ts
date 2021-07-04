cf5c8bed23f5bcd12e71b3769e1fcba1e1ef1b9b
Vinicius Imai <vinicius.imai@gmail.com>
2021-07-04 13:02:42 -0300

    feat: crud grpc para produtos

--- src/products/product-grpc-server/product-grpc-server.controller.ts
+++ src/products/product-grpc-server/product-grpc-server.controller.ts
@@ -1,4 +1,60 @@
 import { Controller } from '@nestjs/common';
+import { GrpcMethod, RpcException } from '@nestjs/microservices';
+import { Metadata, ServerUnaryCall, status } from 'grpc';
+import { EntityNotFoundError } from 'typeorm';
+import { CreateProductDto } from '../dto/create-product.dto';
+import { ProductsService } from '../products.service';
 
-@Controller('product-grpc-server')
-export class ProductGrpcServerController {}
+@Controller()
+export class ProductGrpcServerController {
+  constructor(private productsService: ProductsService) {}
+
+  @GrpcMethod('ProductService')
+  create(
+    data: CreateProductDto,
+    metadata: Metadata,
+    call: ServerUnaryCall<any>,
+  ) {
+    console.log(data);
+    return this.productsService.create(data);
+  }
+
+  @GrpcMethod('ProductService')
+  update(
+    data: { id: number; name: string; price: number },
+    metadata: Metadata,
+    call: ServerUnaryCall<any>,
+  ) {
+    const { id, ...rest } = data;
+    return this.productsService.update(id, data);
+  }
+
+  @GrpcMethod('ProductService')
+  findOne(data: { id: number }) {
+    const { id } = data;
+    return this.productsService.findOne(id);
+  }
+
+  @GrpcMethod('ProductService')
+  async findAll() {
+    const products = await this.productsService.findAll();
+    return { data: products };
+  }
+
+  @GrpcMethod('ProductService', 'Delete')
+  async remove(data: { id: number }) {
+    const { id } = data;
+    //return await this.productsService.remove(id);
+
+    try {
+      return await this.productsService.remove(id);
+    } catch (e) {
+      if (e instanceof EntityNotFoundError) {
+        throw new RpcException({
+          message: 'Product not found',
+          code: status.NOT_FOUND,
+        });
+      }
+    }
+  }
+}
